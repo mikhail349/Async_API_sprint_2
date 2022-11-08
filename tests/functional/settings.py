c0290f66b935538f73398443be219eaeb6669635
Anna <sudnitsina@gmail.com>
2022-11-08 17:10:23 +0300

    Добавлены настройки в settings.py

---
+++ tests/functional/settings.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+
+
+class TestSettings(BaseSettings):
+    ELASTIC_HOST: str = "127.0.0.1"
+    """HOST для подклчючения к ElasticSearch."""
+    ELASTIC_PORT: int = 9200
+    """PORT для подклчючения к ElasticSearch."""
+
+    REDIS_HOST: str = "127.0.0.1"
+    """HOST для подклчючения к Redis."""
+    REDIS_PORT: int = 6379
+    """PORT для подклчючения к Redis."""
+
+
+test_settings = TestSettings()
