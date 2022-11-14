577211a055197d824c056fde42ae64c1bf639a01
mikhail349ya <mikhail349349@yandex.ru>
2022-11-14 19:49:46 +0300

    Вместо фикстуры es_write_data добавлена новая es_data

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -2,6 +2,7 @@ import asyncio
 
 import pytest
 from elasticsearch import AsyncElasticsearch, helpers
+from elasticsearch.exceptions import NotFoundError
 from pydantic import BaseModel
 
 from tests.functional import settings
@@ -31,18 +32,33 @@ async def es():
 
 
 @pytest.fixture
-def es_write_data(es):
-    """Фикстура для записи данных в slastic."""
+def es_data(es):
+    """Получить конструктор для добавления и удаления данных в ES."""
+    def inner(index: str, data: list[BaseModel]):
+        class DataHandler:
 
-    async def inner(index: str, data: list[BaseModel]):
-        query = [
-            {"_index": index, "_id": i.id, "_source": dict(i)}
-            for i in data
-        ]
-        rows_count, errors = await helpers.async_bulk(es, query, refresh=True)
-        if errors:
-            raise Exception("Ошибка записи в Elasticsearch")
+            def __init__(self, index: str, data: list[BaseModel], es_client: AsyncElasticsearch) -> None:
+                self.index = index
+                self.data = data
+                self.es_client = es_client
+            
+            async def insert(self):
+                query = [
+                    {"_index": self.index, "_id": i.id, "_source": dict(i)}
+                    for i in self.data
+                ]
+                _, errors = await helpers.async_bulk(self.es_client, query, refresh=True)
+                if errors:
+                    raise Exception("Ошибка записи в Elasticsearch")
 
+            async def delete(self):
+                try:
+                    for item in self.data:
+                        await self.es_client.delete(id=item.id, index=self.index)
+                except NotFoundError:
+                    pass
+
+        return DataHandler(index=index, data=data, es_client=es)
     return inner
 
 
@@ -53,32 +69,40 @@ async def api_client():
 
 
 @pytest.fixture
-async def genre(es_write_data):
-    """Создание жанра в базе."""
+async def genre(es_data):
+    """Создание и последующее удаление жанра в базе."""
     genre_data = generate_random_genre()
-    await es_write_data("genres", [genre_data])
+    es = es_data("genres", [genre_data])
+    await es.insert()
     yield genre_data
+    await es.delete()
 
 
 @pytest.fixture
-async def genres(es_write_data):
-    """Создание 100 жанров в базе."""
+async def genres(es_data):
+    """Создание и последующее удаление 100 жанров в базе."""
     genres_data = [generate_random_genre() for _ in range(100)]
-    await es_write_data("genres", genres_data)
+    es = es_data("genres", genres_data)
+    await es.insert()
     yield genres_data
+    await es.delete()
 
 
 @pytest.fixture
-async def person(es_write_data):
-    """Создание персоны в базе."""
+async def person(es_data):
+    """Создание и последующее удаление персоны в базе."""
     person_data = generate_random_person()
-    await es_write_data("persons", [person_data])
+    es = es_data("persons", [person_data])
+    await es.insert()
     yield person_data
+    await es.delete()
 
 
 @pytest.fixture
-async def persons(es_write_data):
-    """Создание 100 персон в базе."""
+async def persons(es_data):
+    """Создание и последующее удаление 100 персон в базе."""
     persons_data = [generate_random_person() for _ in range(100)]
-    await es_write_data("persons", persons_data)
+    es = es_data("persons", persons_data)
+    await es.insert()
     yield persons_data
+    await es.delete()
