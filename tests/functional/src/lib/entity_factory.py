25ea3c2fb155409e9c6846a51957af39f4a113bf
mikhail349ya <mikhail349349@yandex.ru>
2022-11-14 22:03:25 +0300

    Добавлена возможность указать name при генерации Жанра и Персоны

--- tests/functional/src/lib/entity_factory.py
+++ tests/functional/src/lib/entity_factory.py
@@ -9,18 +9,30 @@ from tests.functional.src.lib.models.person import Person
 fake = Faker()
 
 
-def generate_random_genre() -> Genre:
-    """Возвращает рандомный объект Genre."""
+def generate_random_genre(name: str = None) -> Genre:
+    """Возвращает рандомный объект Genre.
+    
+    Args:
+        name: название (по умолчанию - сгенерировать)
+    
+    """
     return Genre(
-        id=str(uuid.uuid4()), name=fake.word(), description=fake.text()
+        id=str(uuid.uuid4()),
+        name=name or fake.word(),
+        description=fake.text()
     )
 
 
-def generate_random_person() -> Person:
-    """Возвращает рандомный объект Person."""
+def generate_random_person(name: str = None) -> Person:
+    """Возвращает рандомный объект Person.
+    
+    Args:
+        name: имя (по умолчанию - сгенерировать)
+    
+    """
     return Person(
         id=str(uuid.uuid4()),
-        full_name=fake.name(),
+        full_name=name or fake.name(),
         roles=[random.choice(["actor", "writer", "director"])],
         film_ids=[str(uuid.uuid4())],
     )
