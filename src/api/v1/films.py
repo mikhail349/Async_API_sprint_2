8e90120c74ca0bbed599d30a49518e36df3e6d23
mikhail349 <mikhail349349@gmail.com>
2022-12-13 12:14:11 +0300

    Добавлена ручка /api/v1/films/newest

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -1,11 +1,15 @@
 from http import HTTPStatus
+import datetime
 
 from fastapi import APIRouter, Depends, HTTPException, Query, Path
 
 from src.api.v1.query_params.base import Page, get_page
 from src.api.v1.query_params.films import Filter, get_filter
 from src.api.v1.models.film import FilmList, FilmDetails
+from src.models.user import User
 from src.services.film import FilmService, get_film_service
+from src.services.auth import permission_required
+from src.storages.films import FilmStorageFilter
 from src.core.messages import FILM_NOT_FOUND
 
 router = APIRouter()
@@ -23,6 +27,24 @@ async def search(
     film_service: FilmService = Depends(get_film_service)
 ) -> list[FilmList]:
     films = await film_service.search(query=query, page=page, sort=sort)
+    return [FilmList(**film.dict()) for film in films]
+
+
+@router.get('/newest',
+            response_model=list[FilmList],
+            summary='Список кинопроизведений-новинок',
+            description='Список кинопропроизведений-новинок с фильтрацией '
+                        'по актерам, режиссерам, сценаристам и жанрам '
+                        'с пагинацией и сортировкой')
+async def films(
+    filter: Filter = Depends(get_filter),
+    page: Page = Depends(get_page),
+    sort: list[str] = Query(default=[], description='Поле для сортировки'),
+    film_service: FilmService = Depends(get_film_service),
+    #user: User = Depends(permission_required('view_newest_movies'))
+) -> list[FilmList]:
+    storage_filter = FilmStorageFilter(**filter.dict(), newest=True)
+    films = await film_service.get(filter=storage_filter, page=page, sort=sort)
     return [FilmList(**film.dict()) for film in films]
 
 
@@ -53,5 +75,6 @@ async def films(
     sort: list[str] = Query(default=[], description='Поле для сортировки'),
     film_service: FilmService = Depends(get_film_service)
 ) -> list[FilmList]:
-    films = await film_service.get(filter=filter, page=page, sort=sort)
+    storage_filter = FilmStorageFilter(**filter.dict())
+    films = await film_service.get(filter=storage_filter, page=page, sort=sort)
     return [FilmList(**film.dict()) for film in films]
